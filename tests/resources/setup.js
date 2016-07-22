1f718a732e4be3c1c6858b3f23e933058dd512ef
Kovács Péter <peter@kwep.me>
2016-07-22 15:10:12 +0200

    Synthetic data moved to setup file for tests

---
+++ tests/resources/setup.js
@@ -0,0 +1,106 @@
+/**
+ * Create client
+ * @return {Client}
+ */
+exports.createClient = function (Szamlazz) {
+  return new Szamlazz.Client({
+    user: 'USERNAME',
+    password: 'PASSWORD',
+    eInvoice: false,
+    passpharase: '',
+    requestInvoiceDownload: true,
+    downloadedInvoiceCount: 0,
+    responseVersion: 1
+  })
+}
+
+/**
+ * Create seller
+ * Optional and can be used to override the default data.
+ * @return {Seller}
+ */
+exports.createSeller = function (Szamlazz) {
+  return new Szamlazz.Seller({
+    bank: {
+      name: 'Test Bank <name>',
+      accountNumber: '11111111-11111111-11111111'
+    },
+    email: {
+      replyToAddress: 'test@email.com',
+      subject: 'Invocie email subject',
+      message: 'This is an email message'
+    },
+    issuerName: ''
+  })
+}
+
+/**
+ * Create Buyer
+ * Required, you should supply basic data: name, zip, city, address as a minimum. Hungary is the default country.
+ * @return {Buyer}
+ */
+exports.createBuyer = function (Szamlazz) {
+  return new Szamlazz.Buyer({
+    name: 'Test ' + Math.random(),
+    country: '',
+    zip: '1234',
+    city: 'City',
+    address: 'Some street address',
+    taxNumber: '12345678-1-42',
+    postAddress: {
+      name: 'Some Buyer Name',
+      zip: '1234',
+      city: 'City',
+      address: 'Some street address'
+    },
+    issuerName: '',
+    identifier: 1,
+    phone: '',
+    comment: ''
+  })
+}
+
+/**
+ * Create sold item with net price
+ * @return {Item}
+ */
+exports.createSoldItemNet = function (Szamlazz) {
+  return new Szamlazz.Item({
+    label: 'First item',
+    quantity: 2,
+    unit: 'qt',
+    vat: 27, // can be a number or a special string
+    netUnitPrice: 100.55, // calculates gross and net values from per item net
+    comment: 'An item'
+  })
+}
+
+/**
+ * Create sold item with gross price
+ * @return {Item}
+ */
+exports.createSoldItemGross = function (Szamlazz) {
+  return new Szamlazz.Item({
+    label: 'Second item',
+    quantity: 5,
+    unit: 'qt',
+    vat: 27,
+    grossUnitPrice: 1270 // calculates net and total values from per item gross
+  })
+}
+
+/**
+ * Create invoice
+ * Buyer and seller can be shared between invoices.
+ * @return {Invoice}
+ */
+exports.createInvoice = function (Szamlazz, seller, buyer, items) {
+  return new Szamlazz.Invoice({
+    paymentMethod: Szamlazz.PaymentMethod.BankTransfer,
+    currency: Szamlazz.Currency.Ft,
+    language: Szamlazz.Language.Hungarian,
+    seller: seller,
+    buyer: buyer,
+    items
+  })
+}
--- tests/service.spec.js
+++ tests/service.spec.js
@@ -10,6 +10,8 @@ const sinon = require('sinon')
 const mockery = require('mockery')
 const expect = require('chai').expect
 
+const setup = require('./resources/setup')
+
 let requestStub
 
 let client
@@ -19,7 +21,7 @@ let soldItem1
 let soldItem2
 let invoice
 
-let InvoiceModule
+let Szamlazz
 
 beforeEach(function (done) {
   mockery.enable({
@@ -33,102 +35,13 @@ beforeEach(function (done) {
 
   mockery.registerMock('request', requestStub)
 
-  InvoiceModule = require('../index.js')
-
-  /**
-   * Creates a client
-   * @type {Client}
-   */
-  client = new InvoiceModule.Client({
-    user: 'USERNAME',
-    password: 'PASSWORD',
-    eInvoice: false,
-    passpharase: '',
-    requestInvoiceDownload: true,
-    downloadedInvoiceCount: 0,
-    responseVersion: 1
-  })
-
-  /**
-   * Creates a Seller
-   * Optional and can be used to override the default data.
-   * @type {Seller}
-   */
-  seller = new InvoiceModule.Seller({
-    bank: {
-      name: 'Test Bank <name>',
-      accountNumber: '11111111-11111111-11111111'
-    },
-    email: {
-      replyToAddress: 'test@email.com',
-      subject: 'Invocie email subject',
-      message: 'This is an email message'
-    },
-    issuerName: ''
-  })
-
-  /**
-   * Creates a Buyer
-   * Required, you should supply basic data: name, zip, city, address as a minimum. Hungary is the default country.
-   * @type {Buyer}
-   */
-  buyer = new InvoiceModule.Buyer({
-    name: 'Test ' + Math.random(),
-    country: '',
-    zip: '1234',
-    city: 'City',
-    address: 'Some street address',
-    taxNumber: '12345678-1-42',
-    postAddress: {
-      name: 'Some Buyer Name',
-      zip: '1234',
-      city: 'City',
-      address: 'Some street address'
-    },
-    issuerName: '',
-    identifier: 1,
-    phone: '',
-    comment: ''
-  })
-
-  /**
-   * Creates an item
-   * @type {Item}
-   */
-  soldItem1 = new InvoiceModule.Item({
-    label: 'First item',
-    quantity: 2,
-    unit: 'qt',
-    vat: 27, // can be a number or a special string
-    netUnitPrice: 100.55, // calculates gross and net values from per item net
-    comment: 'An item'
-  })
-
-  /**
-   * Creates another item
-   * @type {Item}
-   */
-  soldItem2 = new InvoiceModule.Item({
-    label: 'Second item',
-    quantity: 5,
-    unit: 'qt',
-    vat: 27,
-    grossUnitPrice: 1270 // calculates net and total values from per item gross
-  })
-
-  /**
-   * Creates an invoice
-   * Buyer and seller can be shared between invoices.
-   * @type {Invoice}
-   */
-  invoice = new InvoiceModule.Invoice({
-    paymentMethod: InvoiceModule.PaymentMethod.BankTransfer,
-    currency: InvoiceModule.Currency.Ft,
-    language: InvoiceModule.Language.Hungarian,
-    seller: seller,
-    buyer: buyer,
-    items: [ soldItem1, soldItem2 ]
-  })
+  Szamlazz = require('..')
+  client = setup.createClient(Szamlazz)
+  seller = setup.createSeller(Szamlazz)
+  buyer = setup.createBuyer(Szamlazz)
+  soldItem1 = setup.createSoldItemNet(Szamlazz)
+  soldItem2 = setup.createSoldItemGross(Szamlazz)
+  invoice = setup.createInvoice(Szamlazz, seller, buyer, [soldItem1, soldItem2])
 
   done()
 })
@@ -355,12 +268,12 @@ describe('Invoice', function () {
     })
 
     it('should set seller', function (done) {
-      expect(invoice._options).to.have.property('seller').to.be.an.instanceof(InvoiceModule.Seller)
+      expect(invoice._options).to.have.property('seller').to.be.an.instanceof(Szamlazz.Seller)
       done()
     })
 
     it('should set buyer', function (done) {
-      expect(invoice._options).to.have.property('buyer').to.be.an.instanceof(InvoiceModule.Buyer)
+      expect(invoice._options).to.have.property('buyer').to.be.an.instanceof(Szamlazz.Buyer)
       done()
     })
 
