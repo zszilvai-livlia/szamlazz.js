819d7024b2c02f4ebe796970deabfd9984962a69
László Monda <laci@monda.hu>
2019-09-18 01:35:20 +0200

    Ditch babel and the ES5 version.

--- CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Contributing
-
-If you make a change to a file in the `src` directory, make sure to run `npm run compile`, so that the es5 version will be updated, too. You can make a pull request afterward.
--- es5/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _modules = ['Buyer', 'Client', 'Invoice', 'Item', 'Seller'];
-
-_modules.forEach(function (n) {
-  exports[n] = require('./lib/' + n);
-});
-
-require('./lib/Constants').setup(exports);
