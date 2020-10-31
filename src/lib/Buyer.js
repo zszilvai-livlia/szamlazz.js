dc2bd565e371cc8bbe2761e357d3433a44585e0a
Zoltan Szilvai <zoltan.szilvai92@gmail.com>
2020-10-31 20:41:13 +0100

    adoalany field added

--- src/lib/Buyer.js
+++ src/lib/Buyer.js
@@ -36,6 +36,7 @@ class Buyer {
       [ 'cim', this._options.address ],
       [ 'email', this._options.email ],
       [ 'sendEmail', this._options.sendEmail ],
+      [ 'adoalany', this._options.taxPayer ],
       [ 'adoszam', this._options.taxNumber ],
       [ 'adoszamEU', this._options.taxNumberEU ],
       [ 'postazasiNev', this._options.postAddress.name ],
